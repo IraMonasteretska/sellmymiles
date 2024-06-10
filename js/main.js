0d99033aa5f48230a4422e6063be6b981a07c667
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-06-10 16:05:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // styled selects
     if ($('select').length) {
         $('.styledselect').select2({
@@ -15,6 +15,54 @@ $(document).ready(function(){
             $('.header').removeClass('scroll');
         }
     });
+
+    // hover plane anim
+    const bx1 = document.querySelector('.bx1');
+    const bx2 = document.querySelector('.bx2');
+    const bx3 = document.querySelector('.bx3');
+    const stepssectPlane = document.querySelector('.stepssect__plane');
+
+    bx1.addEventListener('mouseenter', function () {
+        stepssectPlane.classList.add('hov1');
+    });
+
+    bx1.addEventListener('mouseleave', function () {
+        stepssectPlane.classList.remove('hov1');
+    });
+
+    bx2.addEventListener('mouseenter', function () {
+        stepssectPlane.classList.add('hov2');
+    });
+
+    bx2.addEventListener('mouseleave', function () {
+        stepssectPlane.classList.remove('hov2');
+    });
+
+    bx3.addEventListener('mouseenter', function () {
+        stepssectPlane.classList.add('hov3');
+    });
+
+    bx3.addEventListener('mouseleave', function () {
+        stepssectPlane.classList.remove('hov3');
+    });
+
+    // Testimonials
+    var swiper = new Swiper(".testimsl", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+
 
 
 })
